82bd9a0090cbbec3e41f62d1ab8e5a75257a275a
matin0014 <matinraoufi@outlook.com>
2024-07-07 20:14:17 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+ import inquirer from "inquirer";
+ import qr from "qr-image";
+ import fs from "fs";
+ inquirer
+  .prompt([
+    {message: "type in your url: ",
+        name: "URL",  
+    }
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+  })
+  .catch((error) => {
+    console.log("Error");
+  });
